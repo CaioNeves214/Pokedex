b480d84d9bb704dbe1efd39e4998890a3517376f
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-05-07 21:35:16 -0300

    Estrutura inicial da Pokedex

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return (
+    <div>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/100.png"
+        alt="Pikachu"
+      />
+      <h2>#025 — Voltorb</h2>
+      <p>Tipo: Elétrico</p>
+    </div>
+  )
+}
+
+export default PokemonCard
